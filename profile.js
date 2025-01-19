89776cf1ef113c67ced1956c4abe8822f83890c5
Jacob <jacobstudent29@gmail.com>
2025-01-19 18:49:01 +0100

    added gradients to login and register, added input field to login form

--- profile.js
+++ profile.js
@@ -1,9 +1,13 @@
 function loginPressed() {
     var login = document.getElementById("login-form")
     login.classList.toggle("opacity")
+    var login_button = document.getElementById("login-button")
+    login_button.classList.toggle("magenta")
 }
 
 function registerPressed() {
     var register = document.getElementById("register-form")
     register.classList.toggle("opacity")
+    var register_button = document.getElementById("register-button")
+    register_button.classList.toggle("magenta")
 }
