b4dd8abf5e32428686a4a0450381faa1b1b195b0
Jacob <jacobstudent29@gmail.com>
2025-01-19 18:35:16 +0100

    added more classes to login and register buttons, onclick toggle visibility

---
+++ profile.js
@@ -0,0 +1,9 @@
+function loginPressed() {
+    var login = document.getElementById("login-form")
+    login.classList.toggle("opacity")
+}
+
+function registerPressed() {
+    var register = document.getElementById("register-form")
+    register.classList.toggle("opacity")
+}
